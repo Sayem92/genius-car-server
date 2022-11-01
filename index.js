aedb4d66a990e712ebe171906bfe4c549a8124ad
Sayem92 <sayemmiha123@gmail.com>
2022-11-02 00:48:44 +0600

    add get mongo from data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -12,23 +12,46 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lhckmem.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        const serviceCollection = client.db("geniusCar").collection("services");
+        const orderCollection = client.db("geniusCar").collection("orders")
+
+        // all data get mongodb ----------
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        });
+
+       
 
 
 
 
-app.get('/', (req,res)=>{
+
+
+
+
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+run().catch(error => console.log(error))
+
+
+app.get('/', (req, res) => {
     res.send("Genius car server running ")
 });
 
-app.listen(port , ()=>{
-    console.log("genius car server running port: ", port );
+app.listen(port, () => {
+    console.log("genius car server running port: ", port);
 })
 
 
