f9d1d2cbbc364a928b310632a528acaf1cea341f
Sayem92 <sayemmiha123@gmail.com>
2022-11-04 20:43:44 +0600

    add verify jwt, and token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -14,10 +15,36 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verify token jwt----------------
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader.split(' ')[1];
+   
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(401).send({ message: "forbidden access" })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         const serviceCollection = client.db("geniusCar").collection("services");
-        const orderCollection = client.db("geniusCar").collection("orders")
+        const orderCollection = client.db("geniusCar").collection("orders");
+
+        //jwt token---------------------
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            // sudu token ole kicu bujba na tai json a pataw { } di
+            res.send({ token })
+        })
 
         // all data get mongodb ----------
         app.get('/services', async (req, res) => {
@@ -36,25 +63,53 @@ async function run() {
         });
 
         // order api------------------set to mongo
-        app.post('/orders', async (req, res) => {
+        app.post('/orders', verifyJwt, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order)
             res.send(result)
         });
 
         // order api------------------get all mongo
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJwt, async (req, res) => {
+            const decoded = req.decoded;
+            console.log("inside orders api", decoded);
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message : 'unauthorized access'})
+            }
+
             let query = {}
-            
-            if(req.query.email){
+            if (req.query.email) {
                 query = {
-                    email : req.query.email
+                    email: req.query.email
                 }
             }
             const cursor = orderCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
         })
+
+        // update orders single- ----------sudu takle change
+        app.patch('/orders/:id', verifyJwt, async (req, res) => {
+            const id = req.params.id;
+            const status = req.body;
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    status: status
+                }
+            }
+            const result = await orderCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        })
+
+        // delete orders api----------
+        app.delete('/orders/:id', verifyJwt, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
 
 
